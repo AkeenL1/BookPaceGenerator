f50ef97fa33d1975e8e98e996f7b0e6c44767c4e
Akeen Lewis <akeenlewis1@gmail.com>
2024-03-18 21:08:35 -0400

    lots more front end changes, adding in db! also removing the test images

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import base64
+import json
+
 import openai
 import re
 import os
 from flask import Flask, jsonify, render_template, request
+import sqlite3
 
 openai.api_key = ""
 
@@ -13,10 +16,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/book_detail', methods=['GET'])
+def book_detail():
+    book_id = request.args.get('book_id')
+    contents = get_book(book_id)
+    print(contents)
+    return render_template('book_detail.html', book_detail=contents)
+
 @app.route('/data', methods=['POST'])
 def data():
     print("hit")
     out = {}
+    print(request.form['book-title'])
     images = request.files.getlist('files[]')
     image_path = "../images/IMG_1366.jpg"
     directory_path = '../images'
@@ -38,13 +49,21 @@ def data():
                     "role": "user",
                     "content": [
                         {"type": "text", "text": "This is a table of contents, "
-                                                 "you are a computer analysing this table of contents, for each chapter &"
-                                                 "corresponding subchapter you are directed to give what page each chapter is on and the chapter name."
+                                                 "you are a computer analysing this table of contents, for each "
+                                                 "chapter &"
+                                                 "corresponding subchapter you are directed to give what page each "
+                                                 "chapter is on and the chapter name."
                                                  "This should be formatted as a list of comma seperated key:value "
                                                  "pairings with the key as the page number and the value as the "
-                                                 "chapter name, only provide this list. there should be no space in between the key:value. for example"
-                                                 "A chapter named 'The Start' on page 5 would look like '5:'The Start'. Each chapter should be a single string not broken up in any way besides spaces"
-                                                 "Only consider the chapters and subchapters with numbers corresponding to them. Each chapter Name should start and end with a ' with no spaces between them, for example 'Chapter Name' not ' Chapter Name' "
+                                                 "chapter name, only provide this list. there should be no space in "
+                                                 "between the key:value. for example"
+                                                 "A chapter named 'The Start' on page 5 would look like '5:'The "
+                                                 "Start'. Each chapter should be a single string not broken up in any "
+                                                 "way besides spaces"
+                                                 "Only consider the chapters and subchapters with numbers "
+                                                 "corresponding to them. Each chapter Name should start and end with "
+                                                 "a ' with no spaces between them, for example 'Chapter Name' not ' "
+                                                 "Chapter Name'"
                          },
                         {
                             "type": "image_url",
@@ -70,8 +89,65 @@ def data():
                     out[int_key] = cleaned_val.strip()
 
     sorted_dict = {k: out[k] for k in sorted(out)}
+    table_of_contents = json.dumps(sorted_dict)
+    title = request.form['book-title']
+    add_book(title, table_of_contents)
     print(sorted_dict)
-    return sorted_dict
+    return {'status': 'success'}
+
+
+@app.route('/get-books', methods=['GET'])
+def get_all_books():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute('SELECT id, title FROM BOOKS')
+    books = cursor.fetchall()
+    conn.close()
+    return jsonify([dict(row) for row in books])
+
+
+def get_book(book_id):
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute('SELECT contents FROM BOOKS WHERE id = ?', (book_id,))
+    books = cursor.fetchone()
+    conn.close()
+    if books:
+        book_contents = json.loads(books['contents'])
+    else:
+        book_contents = {}
+
+    return book_contents
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row  # This allows accessing the columns by name
+    return conn
+
+
+def init_db():
+    print("hello")
+    conn = get_db_connection()
+    conn.execute(
+        'CREATE TABLE IF NOT EXISTS BOOKS ('
+        'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+        'title TEXT, '
+        'contents DICT)'
+    )
+    conn.commit()
+    conn.close()
+
+
+def add_book(title, content):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO BOOKS (title, contents) VALUES (?, ?)', (title, content))
+    conn.commit()
+    conn.close()
+
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
